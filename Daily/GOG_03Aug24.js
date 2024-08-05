f61136cc76d108758bf3645d47f3248b0dbe94b7
RKY <xraj2023@gmail.com>
2024-08-05 09:42:54 +0530

    shell

---
+++ Daily/GOG_03Aug24.js
@@ -0,0 +1,16 @@
+// // https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/?envType=daily-question&envId=2024-08-03
+// var canBeEqual = function(target, arr) {
+//   target = target.sort();
+//   arr = arr.sort();
+//   if(arr.length !== target.length)
+//       return false;
+//   for(let i =0; i < arr.length; i++) {
+//       if(arr[i] !== target[i]) {
+//           return false;
+//       }
+//   }
+//   return true;
+// };
+
+let frequencyCount1 = {};
+console.log(frequencyCount1[2] || 0 || null);
